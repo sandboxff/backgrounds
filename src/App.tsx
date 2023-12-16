60bdfcb420604ad75554a4b94bd539fc692436bf
sandboxff <153873660+sandboxff@users.noreply.github.com>
2023-12-16 19:55:08 +0100

    Add functionality for feats and flaws

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import backgorunds from './data/backgrounds.json'
+import feats from './data/feats.json'
+import flaws from './data/flaws.json'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import RollForm from './components/RollForm';
 import './App.css'
 
+type characterItem = {
+  id: number,
+  name: string,
+  effect: string
+}
+
 function App() {
+
+  const [mode, setMode] = useState('')
+  const [repeatSwitch, setRepeatSwitch] = useState(false)
   const [name, setName] = useState('???')
   const [description, setDescription] = useState('')
   const [disable, setDisable] = useState(false)
 
-  const getRandomBackground = () => {
-    let bg = backgorunds[Math.floor(Math.random() * backgorunds.length)]
-    if (bg.length > 0) 
-    {
-      bg = bg.toLocaleUpperCase()
+  useEffect(() => {
+    if (mode != '') {
+      setDisable(true)
+      for (let i = 0; i < 100; i++) {
+        setTimeout(() => setState(), Math.pow(i, 2))
+      }
+      setTimeout(() => setDisable(false), 10000)
     }
-    setName(bg)
-      
+  }, [mode, repeatSwitch])
+
+  const getRandomItem = (collection: Array<string> | Array<characterItem>) => {
+    let randomIndex = Math.floor(Math.random() * backgorunds.length)
+    return collection[randomIndex]
   }
-  const rollBackground = () => {
-    setDisable(true)
-    for (let i = 0; i < 100; i++) {
-      setTimeout(() => getRandomBackground(), Math.pow(i, 2))
+
+  const setState = () => {
+    switch (mode) {
+      case 'backgrounds':
+      {
+        const randomItem = getRandomItem(backgorunds) as string
+        setName(randomItem.toLocaleUpperCase())
+        setDescription('')
+        break
+      }
+      case 'feats':
+      {
+        const randomItem = getRandomItem(feats.feats) as characterItem
+        setName(randomItem.name.toLocaleUpperCase())
+        setDescription(randomItem.effect)
+        break
+      }
+      case 'flaws':
+      {
+        const randomItem = getRandomItem(flaws.flaws) as characterItem
+        setName(randomItem.name.toLocaleUpperCase())
+        setDescription(randomItem.effect)
+        break
+      }
+      default:
     }
-    setTimeout(() => setDisable(false), 10000)
+  }
+
+  const rollHandler = (newMode: 'backgrounds' | 'feats' | 'flaws') => {
+    if (mode === newMode) {
+      setRepeatSwitch(!repeatSwitch)
+    }
+    else {
+      setMode(newMode)
+    }
   }
 
   return (
-    <RollForm name={name} description={description} rollFunction={()=>rollBackground()} isDisabled={disable}>
-    </RollForm>
+    <>
+      <RollForm name={name} description={description}>
+      </RollForm>
+    <Container fluid className='d-flex flex-column h-25'>
+      <Row className='justify-content-center d-flex align-items-center mh-20'>
+        <div className="btn-toolbar justify-content-center">
+          
+          <button className="col-2 mx-4 btn btn-secondary" onClick={() => rollHandler('backgrounds')} disabled={disable}>Background</button>
+          <button className="col-2 mx-4 btn btn-secondary" onClick={() => rollHandler('feats')} disabled={disable}>Feat</button>
+          <button className="col-2 mx-4 btn btn-secondary" onClick={() => rollHandler('flaws')} disabled={disable}>Flaw</button>
+        </div>
+      </Row>
+    </Container>
+    </>  
   )
 }
 
--- src/components/RollForm.tsx
+++ src/components/RollForm.tsx
@@ -6,32 +6,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 type RollFormProps = {
     name: string,
     description: string,
-    rollFunction: React.MouseEventHandler<HTMLButtonElement>,
-    isDisabled: boolean
 }
 
 function RollForm(props: RollFormProps) {
     const hasDescription = props.description.trim().length != 0 ? true : false
     return(
-        <Container fluid className='d-flex flex-column h-100'>
+        <Container fluid className='d-flex flex-column h-75'>
             <Row className='justify-content-center d-flex align-items-center mh-20'>
                 <Col className='col d-flex justify-content-center'>
                     <h2>{props.name}</h2>
                 </Col>
             </Row>
             { hasDescription ? (
-            <Row className='justify-content-center d-flex align-items-center mh-20'>
-                <Col className='col d-flex justify-content-center'>
-                <h2>{props.description}</h2>
+            <Row className='justify-content-center mx-4 d-flex align-items-center mh-20'>
+                <Col className='col-8 d-flex justify-content-center'>
+                <p className='m-4'>{props.description}</p>
                 </Col>
             </Row>) : (<div></div>)}
-            <Row className='justify-content-center d-flex align-items-center mh-20'>
-                <Col className='col-2 d-flex justify-content-center'>
-                    <button className="btn btn-primary" onClick={props.rollFunction} disabled={props.isDisabled}>
-                    ROLL
-                    </button>
-                </Col>
-            </Row> 
         </Container>
     )
 }
